a9754dbbdfb103fba71c127fedc40d5adcd83db1
Gpocas <gpocas01@gmail.com>
2025-06-07 16:37:38 -0300

    overwrite screen md

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
    content: ["*.{html,js}"],
    theme: {
+     screens: {
+      'sm': '640px',
+      'md': '1000px',
+      'lg': '1024px',
+     },
      extend: {},
    },
    plugins: [],
